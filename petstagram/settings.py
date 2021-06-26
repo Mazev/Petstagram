3dae6b17c9304fe9572dfcf9cd5a080f8cba5306
Mazev <77510376+Mazev@users.noreply.github.com>
2021-06-27 00:39:10 +0300

    new

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'petstagram.common'
+    'petstagram.common',
+    'petstagram.pets',
+
 ]
 
 MIDDLEWARE = [
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'stefan',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
